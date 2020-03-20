78153b5a852e8b89799aa6c19068cf354c5298c5
Bartosz Trzos <Bartosz.Trzos@ipfdigital.com>
2020-03-20 18:21:55 +0100

    Initial commit

---
+++ pages/cards.vue
@@ -0,0 +1,26 @@
+<template>
+  <main>
+    <h1>
+      {{ currentQuestion ? currentQuestion : 'Draw a question' }}
+    </h1>
+    <button @click="getRandomQuestion">Shuffle</button>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentQuestion: undefined,
+      deck: ['To be or not to be?', 'What is love?', 'Does this work?']
+    }
+  },
+  methods: {
+    getRandomQuestion() {
+      const randomIndex = Math.floor(Math.random() * this.deck.length)
+      this.currentQuestion = this.deck[randomIndex]
+      this.deck.splice(randomIndex, 1)
+    }
+  }
+}
+</script>
