5bfbdffb7c48a6b46f42d7fa24401f10a37a1298
Bartosz Trzos <Bartosz.Trzos@ipfdigital.com>
2020-03-26 16:40:12 +0100

    Create shop and contact pages

---
+++ components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="container fixed inset-x-0 flex justify-between">
+    <div>
+      <nuxt-link to="/">
+        Home
+      </nuxt-link>
+    </div>
+    <div>
+      <nuxt-link to="/cards">
+        Cards
+      </nuxt-link>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  outline: 1px dashed purple;
+}
+</style>
